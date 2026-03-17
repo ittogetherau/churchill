282d67fa64062bdbc74e24839fc831755d33c9af
sayujkuickel <sayujkuickel@gmail.com>
2026-03-17 14:46:34 +0545

    !feat: updated header buttons to render on specific pages only

--- src/components/globals/header-revamped/header-additional-buttons.tsx
+++ src/components/globals/header-revamped/header-additional-buttons.tsx
@@ -1,11 +1,13 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { LayoutDashboard, University } from "lucide-react";
 import Link from "next/link";
+import { useParams } from "next/navigation";
 
 const LINKS = [
   {
     title: "Student Application Portal",
-    href: "/apply-for-course-admission",
+    href: "https://application.churchill.edu.au/",
     icon: LayoutDashboard,
     external: false,
   },
@@ -35,10 +37,22 @@ const renderButton = (link: (typeof LINKS)[number]) => {
   );
 };
 
+const PagesToRenderStdPortalBtnIn = [
+  "offshore-international-student-information",
+  "onshore-international-student-admission-information",
+];
+
 const HeaderAdditionalButtons = () => {
+  const params = useParams();
+  const slugParam = params?.slug;
+
+  const slug = Array.isArray(slugParam) ? slugParam[0] : slugParam;
+
   return (
     <>
-      {renderButton(LINKS[0])}
+      {slug && PagesToRenderStdPortalBtnIn.includes(slug)
+        ? renderButton(LINKS[0])
+        : null}
       {renderButton(LINKS[1])}
     </>
   );
